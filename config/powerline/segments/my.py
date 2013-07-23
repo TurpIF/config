45a4cab11102f1bd7a86cc89db927987c038a450
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-07-23 14:46:16 +0200

    Fix some bugs in my personnal segment of powerline

--- config/powerline/segments/my.py
+++ config/powerline/segments/my.py
@@ -4,11 +4,7 @@ from __future__ import absolute_import
 
 import sh
 
-from powerline.segments.vim import file_directory
-from powerline.theme import requires_segment_info
-
-@requires_segment_info
-def git_status_line(pl, segment_info, head = "", detached = "#", dirty = " +"):
+def git_status_line(pl, head = "", detached = "#", dirty = " +"):
     '''Return the current status of a git branch.
 
     :param str head:
@@ -20,38 +16,36 @@ def git_status_line(pl, segment_info, head = "", detached = "#", dirty = " +"):
 
     Highlight groups used: ``branch_clean``, ``branch_dirty``, ``branch``.
     '''
+
+    # return None
+
+    branch = '(no branch)'
+    scol = ['branch']
+
     try:
-        path = None
-        try:
-            path = file_directory(pl, segment_info, False, False, False)
-        except KeyError:
-            pass
-
-        if path is None:
-            path = segment_info['getcwd']()
-        sh.cd(path)
-        branch = '(no branch)'
-        scol = ['branch']
-
-        try:
-            sh.git('symbolic-ref', 'HEAD')
-            for b in sh.git.branch().split('\n'):
-                if b and  b[0] == '*':
-                    branch = head + ' '.join(b.split(' ')[1:])
-                    break
-        except sh.ErrorReturnCode_128:
-            branch = detached + sh.git('show-ref', '--head', '--abbrev', '-s').split('\n')[0]
-
-        try:
-            sh.git.diff('--quiet')
-            scol = ['branch_clean'] + scol
-        except sh.ErrorReturnCode_1:
-            scol = ['branch_dirty'] + scol
-            branch = branch + dirty
-
-        return [{
-            'contents': branch,
-            'highlight_group': scol,
-            }]
+        sh.git('symbolic-ref', 'HEAD')
+        # Get the used branch
+        for b in sh.git.branch().split('\n'):
+            if b and  b[0] == '*':
+                branch = head + ' '.join(b.split(' ')[1:])
+                break
     except sh.ErrorReturnCode_128:
-        return None
+        # Detached from HEAD
+        try:
+            branch = detached + sh.git('show-ref', '--head', '--abbrev', '-s').split('\n')[0]
+        except sh.ErrorReturnCode_128:
+            # Not a git repo
+            return None
+
+    try:
+        sh.git.diff('--quiet')
+        scol = ['branch_clean'] + scol
+    except sh.ErrorReturnCode_1:
+        # Dirty branch
+        scol = ['branch_dirty'] + scol
+        branch = branch + dirty
+
+    return [{
+        'contents': branch,
+        'highlight_group': scol,
+        }]
